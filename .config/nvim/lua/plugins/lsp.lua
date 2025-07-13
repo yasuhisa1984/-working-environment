de76b8831a82d80609f3954389f1a797b84a34c7
小鍛冶泰久 <yasuhisa@jimihenyasuhisanoMacBook-Pro.local>
2025-07-13 16:35:47 +0900

    再初期化

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,225 @@
+return {
+	-- tools
+	{
+		"williamboman/mason.nvim",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
+				"stylua",
+				"selene",
+				"luacheck",
+				"shellcheck",
+				"shfmt",
+				"tailwindcss-language-server",
+				"typescript-language-server",
+				"css-lsp",
+			})
+		end,
+	},
+
+	-- lsp servers
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			inlay_hints = { enabled = false },
+			---@type lspconfig.options
+			servers = {
+				cssls = {},
+				tailwindcss = {
+					root_dir = function(...)
+						return require("lspconfig.util").root_pattern(".git")(...)
+					end,
+				},
+				intelephense = {
+					root_dir = function(fname)
+						-- composer.json / .git どちらかをプロジェクトルートとする
+						return require("lspconfig.util").root_pattern("composer.json", ".git")(fname) or vim.fn.getcwd()
+					end,
+					settings = {
+						intelephense = {
+							format = { enable = false }, -- フォーマッタは別に任せる場合
+							environment = { includePaths = { "vendor" } },
+							stubs = {
+								"apache",
+								"bcmath",
+								"bz2",
+								"calendar",
+								"com_dotnet",
+								"Core", -- 必須！file_exists, file_get_contentsなど
+								"ctype",
+								"curl",
+								"date",
+								"dom",
+								"exif",
+								"fileinfo",
+								"filter",
+								"ftp",
+								"gd",
+								"gettext",
+								"gmp",
+								"hash",
+								"iconv",
+								"imap",
+								"intl",
+								"json", -- 必須！json_encode, json_decode
+								"ldap",
+								"libxml",
+								"mbstring",
+								"mcrypt",
+								"mysql",
+								"mysqli",
+								"openssl",
+								"pcntl",
+								"pcre",
+								"PDO",
+								"pdo_mysql",
+								"pdo_sqlite",
+								"Phar",
+								"posix",
+								"readline",
+								"Reflection",
+								"session",
+								"SimpleXML",
+								"soap",
+								"sockets",
+								"sodium",
+								"SPL", -- 必須！オブジェクト周り
+								"standard", -- 必須！PHP標準関数
+								"superglobals",
+								"tokenizer",
+								"xml",
+								"xmlreader",
+								"xmlrpc",
+								"xmlwriter",
+								"Zend OPcache",
+								"zip",
+								"zlib",
+							},
+							files = { maxSize = 5000000 }, -- 5 MB
+						},
+					},
+				},
+				tsserver = {
+					root_dir = function(...)
+						return require("lspconfig.util").root_pattern(".git")(...)
+					end,
+					single_file_support = false,
+					settings = {
+						typescript = {
+							inlayHints = {
+								includeInlayParameterNameHints = "literal",
+								includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+								includeInlayFunctionParameterTypeHints = true,
+								includeInlayVariableTypeHints = false,
+								includeInlayPropertyDeclarationTypeHints = true,
+								includeInlayFunctionLikeReturnTypeHints = true,
+								includeInlayEnumMemberValueHints = true,
+							},
+						},
+						javascript = {
+							inlayHints = {
+								includeInlayParameterNameHints = "all",
+								includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+								includeInlayFunctionParameterTypeHints = true,
+								includeInlayVariableTypeHints = true,
+								includeInlayPropertyDeclarationTypeHints = true,
+								includeInlayFunctionLikeReturnTypeHints = true,
+								includeInlayEnumMemberValueHints = true,
+							},
+						},
+					},
+				},
+				html = {},
+				yamlls = {
+					settings = {
+						yaml = {
+							keyOrdering = false,
+						},
+					},
+				},
+				lua_ls = {
+					-- enabled = false,
+					single_file_support = true,
+					settings = {
+						Lua = {
+							workspace = {
+								checkThirdParty = false,
+							},
+							completion = {
+								workspaceWord = true,
+								callSnippet = "Both",
+							},
+							misc = {
+								parameters = {
+									-- "--log-level=trace",
+								},
+							},
+							hint = {
+								enable = true,
+								setType = false,
+								paramType = true,
+								paramName = "Disable",
+								semicolon = "Disable",
+								arrayIndex = "Disable",
+							},
+							doc = {
+								privateName = { "^_" },
+							},
+							type = {
+								castNumberToInteger = true,
+							},
+							diagnostics = {
+								disable = { "incomplete-signature-doc", "trailing-space" },
+								-- enable = false,
+								groupSeverity = {
+									strong = "Warning",
+									strict = "Warning",
+								},
+								groupFileStatus = {
+									["ambiguity"] = "Opened",
+									["await"] = "Opened",
+									["codestyle"] = "None",
+									["duplicate"] = "Opened",
+									["global"] = "Opened",
+									["luadoc"] = "Opened",
+									["redefined"] = "Opened",
+									["strict"] = "Opened",
+									["strong"] = "Opened",
+									["type-check"] = "Opened",
+									["unbalanced"] = "Opened",
+									["unused"] = "Opened",
+								},
+								unusedLocalExclude = { "_*" },
+							},
+							format = {
+								enable = false,
+								defaultConfig = {
+									indent_style = "space",
+									indent_size = "2",
+									continuation_indent_size = "2",
+								},
+							},
+						},
+					},
+				},
+			},
+			setup = {},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = function()
+			local keys = require("lazyvim.plugins.lsp.keymaps").get()
+			vim.list_extend(keys, {
+				{
+					"gd",
+					function()
+						-- DO NOT RESUSE WINDOW
+						require("telescope.builtin").lsp_definitions({ reuse_win = false })
+					end,
+					desc = "Goto Definition",
+					has = "definition",
+				},
+			})
+		end,
+	},
+}
